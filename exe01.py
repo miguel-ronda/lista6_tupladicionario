290111e44fde6435dd7ad04ee7df527d707e9069
miguel-ronda <miguel_ronda@estudante.sesisenai.org.br>
2025-02-21 21:38:01 -0300

    dvcnjdiuv

--- exe01.py
+++ exe01.py
@@ -1,17 +1,11 @@
 #Crie uma tupla contendo os nomes de cinco países e exiba toda a tupla. 
 #Peça ao usuário para inserir um dos países que foram mostrados a ele e, em seguida, exibir o número de índice (ou seja, posição na lista) desse item na tupla.
 print("miguel do amaral paes ronda")
-
-tupla_paises = ("Brasil", "Argentina", "Alemanha", "Japão", "França")
-
-
-print("Tupla de países:", tupla_paises)
-
-pais_selecao = input("Digite o nome de um país da lista acima: ")
-
-
-if pais_selecao in tupla_paises:
-    i = tupla_paises.index(pais_selecao)
-    print("O índice do país {} é: {}".index(tupla_paises,i))
+paises= ('brasil,colombia,italia,marrocos,inglaterra')
+print(paises)
+selecao_paises = (input("digite um país"))
+if selecao_paises in paises:
+    indice = paises.index(selecao_paises)
+    print(" o indice do pais e ", indice)
 else:
-    print("O país inserido não está na lista.")
+    print("PAIS ESTE PAIS NAO ESTA REGISTRADO")
